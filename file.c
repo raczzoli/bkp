fb8bf9855f7a77194535ae5ce9716ed8c3900cd1
Zoltan Racz <racz.zoli@gmail.com>
2025-09-21 21:24:14 +0300

    Added write_file function to file.c/file.h

--- file.c
+++ file.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 #include "file.h"
+
+int write_file(char *path, off_t size, unsigned char *sha1)
+{
+	int fd = open(path, O_RDONLY);
+	if (fd < 0) {
+		fprintf(stderr, "Error opening file %s for backup\n", path);
+		return -1;
+	}
+
+	printf("file %s of size %ld\n", path, size);
+
+	return 0;
+}
+
--- file.h
+++ file.h
@@ -1,6 +1,6 @@
 #ifndef FILE_H
 #define FILE_H
 
-
+int write_file(char *path, off_t size, unsigned char *sha1);
 
 #endif
