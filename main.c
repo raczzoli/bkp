6927e6744b6a888930cd12928908ab1770258365
Zoltan Racz <racz.zoli@gmail.com>
2025-09-22 21:45:30 +0300

    Added command line options to main + help

--- main.c
+++ main.c
@@ -16,12 +16,33 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
 #include <openssl/sha.h>
 
 #include "tree.h"
 #include "sha1.h"
 
-int main()
+static struct option cmdline_options[] = {
+	{"create-snapshot",  no_argument,       0, 0},
+	{"snapshots",        no_argument,       0, 0},
+	{"restore-snapshot", required_argument, 0, 0},
+	{"help", no_argument, 0, 'h'},
+	{0, 0, 0, 0}
+};
+
+static void create_snapshot();
+
+static void print_help();
+static void parse_cmdline_args(int argc, char **argv);
+
+int main(int argc, char **argv)
+{
+	parse_cmdline_args(argc, argv);
+	return 0;
+}
+
+static void create_snapshot()
 {
 	int ret = 0;
 	unsigned char sha1[SHA_DIGEST_LENGTH];
@@ -31,13 +52,57 @@ int main()
 
 	if (ret) {
 		fprintf(stderr, "Error generating tree (code: %d)!\n", ret);
-		return -1;
+		return;
 	}
 
 	sha1_to_hex(sha1, sha1_hex);
 
-	printf("Tree sha1: %s\n", sha1_hex);
-
-	return 0;
+	printf("\nTree sha1: %s\n", sha1_hex);
 }
 
+static void parse_cmdline_args(int argc, char **argv)
+{
+	int opt_idx = 0;
+	int opt = 0;
+
+	while ((opt = getopt_long(argc, argv, "h", cmdline_options, &opt_idx)) != -1) {
+		switch(opt) {
+			case 'h':
+				print_help();
+			break;
+			case 0:
+				if (strcmp(cmdline_options[opt_idx].name, "create-snapshot") == 0) {
+					create_snapshot();
+				}
+				else if (strcmp(cmdline_options[opt_idx].name, "snapshots") == 0) {
+					printf("--snapshots :: to be implemented\n");
+				}
+				else if (strcmp(cmdline_options[opt_idx].name, "restore-snapshot") == 0) {
+					if (!optarg) {
+						printf("Invalid usage of --restore-snapshot [SHA1]");
+						return;
+					}
+					printf("--restore-snapshot %s :: to be implemented\n", optarg);
+				}
+			break;
+			case '?': // unknown option or missing required argument
+				break;
+			default:
+				printf("Invalid command line option!\n");
+		}
+	}
+}
+
+static void print_help()
+{
+	printf("\n");
+    printf("Usage: bkp [OPTIONS]\n");
+    printf("A lightweight and space efficient (incremental) file-level backup utility for Linux.\n\n");
+    printf("Options:\n");
+    printf("  --create-snapshot                               Create a new snapshot of the backed up directory\n");
+    printf("  --snapshots                                     Print a list of snapshots done so far\n");
+    printf("  --restore-snapshot [SHA1] [OUTPUT_DIR]          Restores the snapshot identified by the passed SHA1 to OUTPUT_DIR\n");
+	printf("\n");
+	printf("  -h, --help                                      Show this help message and exit\n");
+	printf("\n");
+}
