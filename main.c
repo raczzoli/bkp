a4af61dbb9ecce26a42fd6b833a76466438210f8
Zoltan Racz <racz.zoli@gmail.com>
2025-09-22 17:32:08 +0300

    Added copyright notice to source files and added COPYING file with the full licence

--- main.c
+++ main.c
@@ -1,3 +1,19 @@
+/* 
+ * Copyright (C) 2025 Zoltán Rácz
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.  
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <openssl/sha.h>
@@ -11,10 +27,10 @@ int main()
 	unsigned char sha1[SHA_DIGEST_LENGTH];
 	char sha1_hex[40+1];
 
-	ret = gen_tree("/home/rng89/workspace/linux/", sha1);
+	ret = gen_tree("/home/rng89/", sha1);
 
 	if (ret) {
-		fprintf(stderr, "Error generating tree!\n");
+		fprintf(stderr, "Error generating tree (code: %d)!\n", ret);
 		return -1;
 	}
 
