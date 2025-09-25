5fba60b3f74882fbfa2bcef57579aa3c870308c7
Zoltan Racz <racz.zoli@gmail.com>
2025-09-25 15:46:32 +0300

    Modified print_sha1_file to close the sha1 file after extracting the type of the file, compute the raw sha1 hash and pass it to respective print functions

--- print-file.c
+++ print-file.c
@@ -1,3 +1,4 @@
+#include <openssl/sha.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -6,16 +7,21 @@
 
 #include "print-file.h"
 #include "tree.h"
+#include "sha1-file.h"
 #include "snapshot.h"
 
 int print_sha1_file(char *sha1_hex)
 {
-	int ret = 0;
 	int bytes = 0;
 	int fd = 0;
 	char path[PATH_MAX];
+	unsigned char sha1[SHA_DIGEST_LENGTH];
 	char ftype[20];
-	struct stat stat;
+
+	if (hex_to_sha1(sha1_hex, sha1)) {
+		fprintf(stderr, "Invalid SHA1 value!\n");
+		return -1;
+	}
 
 	sprintf(path, ".bkp-data/%s", sha1_hex);
 	
@@ -25,41 +31,25 @@ int print_sha1_file(char *sha1_hex)
 		return -1;
 	}
 
-	if (fstat(fd, &stat)) {
-		ret = -1;
-		fprintf(stderr, "fstat error!\n");
-		goto end;
-	}
-
 	bytes = read(fd, ftype, 20);
 	if (bytes < 0) {
-		ret = -1;
 		fprintf(stderr, "Error reading from SHA1 file!\n");
-		goto end;
+		return -1;
 	}
-	
-	int len = strlen(ftype);
-
-	/*
-	 * We position the file cursor rightafter "type\0" so 
-	 * when we pass the fd to the function which needs to 
-	 * parse the respective type, read will be at the 
-	 * position where the file`s actual content starts
-	 */
-	lseek(fd, len+1, SEEK_SET);
+	close(fd);
 
 	printf("----------- %s -----------\n", ftype);
 
 	if (strcmp(ftype, "snapshot") == 0) 
-		print_snapshot_file(fd, &stat);
-	else if (strcmp(ftype, "tree") == 0) 
+		return print_snapshot_file(sha1);
+	/*
+	if (strcmp(ftype, "tree") == 0) 
 		print_tree_file(fd, &stat);
+	*/
 	// blob
 	// chunks
 	// etc.
 
-end:
-	close(fd);
-	return ret;
+	return 0;
 }
 
